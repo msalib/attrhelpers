9f24f3b542378312938bb216a331aa54f80499df
Michael Salib <msalib@indigoag.com>
2019-09-02 22:55:38 -0400

    Fix the package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="attrhelpers-msalib",
+    name="attrhelpers",
     version="0.0.1",
     author="Michael Salib",
     author_email="msalib@gmail.com",
