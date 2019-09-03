28c6deee3e2ede76fa4790f588ebd4174f510650
Michael Salib <msalib@indigoag.com>
2019-09-02 22:58:50 -0400

    And update the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="attrhelpers",
-    version="0.0.1",
+    version="0.0.2",
     author="Michael Salib",
     author_email="msalib@gmail.com",
     description="Type based validators for attrs",
